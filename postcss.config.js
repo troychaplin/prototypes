87d56d477e9bce538e5ba38c44e6cc57446bb0a5
Troy Chaplin <troy.chaplin@gmail.com>
2022-10-15 20:46:59 -0400

    Testing spacing

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,26 @@ module.exports = {
         autoprefixer: {},
     },
 };
+
+// module.exports = {
+//     plugins: [
+//         'tailwindcss',
+//         process.env.NODE_ENV === 'production'
+//             ? [
+//                   '@fullhuman/postcss-purgecss',
+//                   {
+//                       content: [
+//                           './pages/**/*.{js,jsx,ts,tsx}',
+//                           './components/**/*.{js,jsx,ts,tsx}',
+//                           './components/**/**/*.{js,jsx,ts,tsx}',
+//                           './layouts/**/*.{js,jsx,ts,tsx}',
+//                           './utils/**/*.{js,jsx,ts,tsx}',
+//                       ],
+//                       defaultExtractor: (content) =>
+//                           content.match(/[\w-/:]+(?<!:)/g) || [],
+//                   },
+//               ]
+//             : undefined,
+//         'postcss-preset-env',
+//     ],
+// };
